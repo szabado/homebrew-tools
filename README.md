a00808b9bf318a54c48854b931dd3a679dd693df
Felix Jancso-Szabo <felix.jancso-szabo@kik.com>
2018-07-19 10:42:10 -0400

    first attempt at zkcli script

---
+++ README.md
@@ -0,0 +1,15 @@
+# homebrew-tools
+
+Homebrew tap for tools I've created
+
+## add this tap
+
+```
+brew tap fJancsoSzabo/tools
+```
+
+## list available tools
+
+```
+brew search fJancsoSzabo
+```
---
+++ zkcli.rb
@@ -0,0 +1,17 @@
+class Zkcli < Formula
+	homepage "https://github.com/fJancsoSzabo/zkcli"
+	url "https://github.com/fJancsoSzabo/zkcli/archive/v2.0.0-beta.tar.gz"
+	sha256 "830d6f31a69e0f5c73525baf8fb280920b86915afb62e5dceb40001ca0d5f279"
+
+	depends_on "go" => :build
+	depends_on "dep" => :build
+
+	def install
+		system "dep ensure"
+		bin.install ".gobuild/bin/zkcli" => "zkcli"
+	end
+
+	test do
+		system "#{bin}/zkcli", "--help"
+	end
+end
