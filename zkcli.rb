43a213c20c9cf418e211675f7b5f4f23d9e819f4
Felix Jancso-Szabo <felix.jancso-szabo@kik.com>
2018-07-19 11:01:30 -0400

    Added description

--- zkcli.rb
+++ zkcli.rb
@@ -1,17 +1,18 @@
 class Zkcli < Formula
-	homepage "https://github.com/fJancsoSzabo/zkcli"
-	url "https://github.com/fJancsoSzabo/zkcli/archive/v2.0.1-beta.tar.gz"
-	sha256 "93545eacb4125e380252b9dfcc2fe59bab4d76f9417597c2d7a938fdd7b57e06"
+  desc "Non-interactive CLI for Zookeeper"
+  homepage "https://github.com/fJancsoSzabo/zkcli"
+  url "https://github.com/fJancsoSzabo/zkcli/archive/v2.0.1-beta.tar.gz"
+  sha256 "93545eacb4125e380252b9dfcc2fe59bab4d76f9417597c2d7a938fdd7b57e06"
 
-	depends_on "go" => :build
-	depends_on "dep" => :build
+  depends_on "go" => :build
+  depends_on "dep" => :build
 
-	def install
-		system "./gobuild.sh"
-		bin.install ".gobuild/bin/zkcli" => "zkcli"
-	end
+  def install
+    system "./gobuild.sh"
+    bin.install ".gobuild/bin/zkcli" => "zkcli"
+  end
 
-	test do
-		system "#{bin}/zkcli", "--help"
-	end
+  test do
+    system "#{bin}/zkcli", "--help"
+  end
 end
