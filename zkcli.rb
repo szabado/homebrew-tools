abc52c30e6208d2cd1dfd8110574c5fe233bef7d
Felix Jancso-Szabo <felix.ajs@gmail.com>
2019-02-06 19:35:21 -0800

    Update zkcli.rb

--- zkcli.rb
+++ zkcli.rb
@@ -1,7 +1,7 @@
 class Zkcli < Formula
   desc "Non-interactive CLI for Zookeeper"
-  homepage "https://github.com/fJancsoSzabo/zkcli"
-  url "https://github.com/fJancsoSzabo/zkcli/archive/v2.0.1-beta.tar.gz"
+  homepage "https://github.com/szabado/zkcli"
+  url "https://github.com/szabado/zkcli/archive/v2.0.1-beta.tar.gz"
   sha256 "93545eacb4125e380252b9dfcc2fe59bab4d76f9417597c2d7a938fdd7b57e06"
 
   depends_on "go" => :build
