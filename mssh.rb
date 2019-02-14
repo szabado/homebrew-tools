8d63c0d0a833a2330ab90db2a00605857832b2b8
Felix Jancso-Szabo <felix.ajs@gmail.com>
2019-02-14 00:03:51 -0800

    Update mssh.rb

--- mssh.rb
+++ mssh.rb
@@ -2,7 +2,7 @@ class Mssh < Formula
   desc "A tool for running multiple commands and ssh jobs in parallel, and easily collecting the results."
   homepage "https://github.com/szabado/mssh"
   url "https://github.com/szabado/mssh/archive/v0.1.1.tar.gz"
-  sha256 "41f3601e2829005fc84604b817488463ae9f2b5f432d43c76d98f9eefbbe326c"
+  sha256 "1ddadf2bcd5bb0df1ee287ae2c45555c601aef1b576bd188d8903ff4770f35f6"
 
   depends_on "go" => :build
 
